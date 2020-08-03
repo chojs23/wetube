7f768df58f3b3213e72d5f76646f30e500faa015
chojs23 <chojs990222@gmail.com>
2020-08-03 19:22:42 +0900

    MVC pattern

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 //const express = require('express'); //node moudule import
 
 const app = express(); //execute
@@ -14,17 +17,17 @@ const app = express(); //execute
 //     console.log(`Listening on: http://localhost:${PORT}`);
 // }
 
-function handleHome(req,res){
+// function handleHome(req,res){
     
-    res.send('Hello from home');
-}
+//     res.send('Hello from home');
+// }
 
-const handleProfile = (req,res) => res.send("You are on my profile")
-//middleware -> next는 다음에 실행할 함수
-const betweenHome =(req,res,next)=>{
-    console.log('Between');
-    next();
-};
+// const handleProfile = (req,res) => res.send("You are on my profile")
+// //middleware -> next는 다음에 실행할 함수
+// const betweenHome =(req,res,next)=>{
+//     console.log('Between');
+//     next();
+// };
 
 
 //app.use -> 모든 route에서 middleware 실행
@@ -34,11 +37,15 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet());
 app.use(morgan("common"));//morgan -> 기록함
 
+//app.use("/",globalRouter);
+app.use(routes.home,globalRouter);
+//app.use("/users",userRouter);
+app.use(routes.users,userRouter);
+//app.use("/videos",videoRouter);
+app.use(routes.videos,videoRouter);
 
 //app.get("/",betweenHome,handleHome);//betweenhome 다음 handleHome 실행
-app.get("/",handleHome);
 
-app.get("/profile",handleProfile);
 
 app.use("/user",userRouter);
 //app.listen(PORT,handleListening);//when you start listening call handleListening
