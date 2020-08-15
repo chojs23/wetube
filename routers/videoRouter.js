3f62cec2267952b333cb4f159980412509db7b25
chojs23 <chojs990222@gmail.com>
2020-08-15 19:18:14 +0900

    uploading video on database

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,20 +1,21 @@
 import express from "express";
 import routes from "../routes";
 import {
-    videos,
-    postUpload,
-    getUpload,
-    videoDetail,
-    editVideo,
-    deleteVideo
-  } from "../controllers/videoController";
+  videos,
+  postUpload,
+  getUpload,
+  videoDetail,
+  editVideo,
+  deleteVideo,
+} from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 const videoRouter = express.Router();
 
 //videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
-export default videoRouter; //export default 는 파일로 전체를export함 
-                            //그냥 export는 오직 한 변수만 export
+export default videoRouter; //export default 는 파일로 전체를export함
+//그냥 export는 오직 한 변수만 export
