1a5ba86fd09737a77b856ee917e6a81b0988da11
chojs23 <chojs990222@gmail.com>
2020-08-18 19:20:17 +0900

    making styles

--- middlewares.js
+++ middlewares.js
@@ -6,7 +6,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube"; // locals에 로컬변수 저장 이 변수들을 템플릿에서 사용가능
   res.locals.routes = routes; // 전역으로 사용하는 변수추가
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 3,
   };
   next();
